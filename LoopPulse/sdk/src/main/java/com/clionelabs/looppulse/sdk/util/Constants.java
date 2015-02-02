ffac9a083d8f199d3014306e445d5de6feabcc86
HiuKim <hiukim@hiukim.local>
2015-02-02 16:23:01 +0800

    Create a Constant class for putting settings together

---
+++ LoopPulse/sdk/src/main/java/com/clionelabs/looppulse/sdk/util/Constants.java
@@ -0,0 +1,8 @@
+package com.clionelabs.looppulse.sdk.util;
+
+/**
+ * Created by hiukim on 2015-02-02.
+ */
+public class Constants {
+    public static final String AUTH_URL = "http://beta.looppulse.com/api/authenticate/applications/";
+}
