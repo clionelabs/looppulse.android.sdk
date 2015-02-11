d4ebd520448fdb353102e97896c3f30abb172594
HiuKim <hiukim@hiukim.local>
2015-02-11 17:51:21 +0800

    Two demo apps for eztable and 91mai

---
+++ LoopPulse/samples/tw91mai/src/androidTest/java/com/clionelabs/looppulse/samples/tw91mai/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.clionelabs.looppulse.samples.tw91mai;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
--- LoopPulse/sdk/src/main/java/com/clionelabs/looppulse/sdk/util/Constants.java
+++ LoopPulse/sdk/src/main/java/com/clionelabs/looppulse/sdk/util/Constants.java
@@ -4,5 +4,6 @@ package com.clionelabs.looppulse.sdk.util;
  * Created by hiukim on 2015-02-02.
  */
 public class Constants {
-    public static final String AUTH_URL = "http://beta.looppulse.com/api/authenticate/applications/";
+//    public static final String AUTH_URL = "http://beta.looppulse.com/api/authenticate/applications/";
+public static final String AUTH_URL = "http://localhost:8010/api/authenticate/applications/";
 }
